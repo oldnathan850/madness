f2061aeb497fb502632860bc7927b95d310196d3
oldnathan850 <43620293+oldnathan850@users.noreply.github.com>
2018-09-28 19:56:44 +0300

    Update nath.js

--- nath.js
+++ nath.js
@@ -20,7 +20,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+        message.channel.send(`nathan ${x}`)
           .then(m => {
             count++;
           })
