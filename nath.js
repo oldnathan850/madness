8e194e208499ff55540e9d2a0659f232d4b5f420
oldnathan850 <43620293+oldnathan850@users.noreply.github.com>
2018-09-28 16:24:23 +0300

    Add files via upload

---
+++ nath.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`https://discord.gg/jmm8jGK`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`https://discord.gg/jmm8jGK`) 
+}).catch(console.error)
+})
+client.login(process.env.BOT_TOKEN);
