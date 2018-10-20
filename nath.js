d0c3c31e481a1966eff0ee47d0883dd945587ae6
oldnathan850 <43620293+oldnathan850@users.noreply.github.com>
2018-10-20 20:43:25 +0300

    Update nath.js

--- nath.js
+++ nath.js
@@ -5,27 +5,14 @@ console.log("Welcome");
  
  client.on("guildMemberRemove", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`https://discord.gg/jmm8jGK`)
+  return channel.send(`https://discord.gg/MZMjM5d`)
 }).catch(console.error)
 
 })
  
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
-  return channel.send(`https://discord.gg/jmm8jGK`) 
+  return channel.send(`https://discord.gg/MZMjM5d`) 
 }).catch(console.error)
 })
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`nathan ${x}`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
 client.login(process.env.BOT_TOKEN);
